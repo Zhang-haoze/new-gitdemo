23f6a695248be31ef44b96c44db6909a679afc3d
Zhang-haoze <18377269@buaa.edu.cn>
2020-03-27 10:09:07 +0800

    second updata fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -3,3 +3,15 @@ Created on Wed Apr 24 21:14:04 2019
 
 @author: Zhang-haoze
 """
+
+a = int(input())   #输入大佬的步长
+b = int(input())    #输入助教x的步长
+m=a*b             #先将二者相乘，以便进行后续计算
+def s(a,b):         #建立一个递归函数
+    if a%b!=0:     #利用欧几里得算法算最大公约数
+        c=a%b
+        return s(b,c)
+    else:
+        return b
+    
+    
